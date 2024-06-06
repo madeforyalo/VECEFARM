1c5cd06320c9e48890174270b8b518481daed48f
madeforyalo <madeforyalo@gmail.com>
2024-06-06 11:45:06 -0300

    validacion del loggin y el index

--- loggin.php
+++ loggin.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   $nombre=$_GET['usuario'];
   $contra=$_GET['contra'];
 
@@ -13,35 +14,33 @@
 
   if(mysqli_affected_rows($conn)>0){
 
-    if($contra==$registro['emp_pass']){
-      session_start();
-      $_SESSION['id']=$registro['emp_id'];
-      $_SESSION['nombre']=$registro['emp_nom'];
-      $_SESSION['tipoUsuario']=$registro['rol_id'];
+    if($nombre==$registro['emp_usu']){
+      if($contra==$registro['emp_pass']){
+        $_SESSION['id']=$registro['emp_id'];
+        $_SESSION['nombre']=$registro['emp_nom'];
+        $_SESSION['tipoUsuario']=$registro['rol_id'];
 
-      switch($registro['rol_id']){
-        case 1:
-          header("Location:./admin.php");
-          break;
-        case 2:
-          header("Location:./empleado.php");
-          break;
-        default:
-        break;          
-      }
+        switch($registro['rol_id']){
+          case 1:
+            header("Location:./admin.php");
+            break;
+          case 2:
+            header("Location:./empleado.php");
+            break;
+          default:
+          break;          
+        }
+    }else {
+      $_SESSION['mensaje'] = 'El Usuario y/o Contraseña son incorrectos';
+      $_SESSION['tipo_mensaje'] = 'danger';
+      header("location:index.php");        
     }
-
-    else {
-          
-            header("location:index.php?pass");
-            
-        
-    }
-
   }
-  else {
-      
-            header("location:index.php?usu");
-  }
+}
+   else {
+    $_SESSION['mensaje'] = 'El Usuario y/o Contraseña son incorrectos';
+    $_SESSION['tipo_mensaje'] = 'danger';
+    header("location:index.php");
+   }
 
 ?>
