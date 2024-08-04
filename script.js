a998ba2d856117ac22cecd60216c491c3e49ce72
AlenOzcariz <alenozcariz07@gmail.com>
2024-08-04 11:53:14 -0300

    Mejore la validación del login, agregue Function Bloq Mayus

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+//funcion mayusculas activadas
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('floatingPassword');
+    const capsLockMessage = document.getElementById('caps-lock-message');
+
+    function checkCapsLock(event) {
+        const isCapsLock = event.getModifierState && event.getModifierState('CapsLock');
+        if (isCapsLock) {
+            capsLockMessage.style.display = 'flex';
+        } else {
+            capsLockMessage.style.display = 'none';
+        }
+    }
+
+    passwordInput.addEventListener('keydown', checkCapsLock);
+    passwordInput.addEventListener('keyup', checkCapsLock);
+});
 
